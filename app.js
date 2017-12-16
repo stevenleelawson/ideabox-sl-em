0b2ac94bd4c0f639cd1cca9ae50fa49a377bda03
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2017-12-16 10:23:56 -0700

    Add basic prepend idea functionality

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+var $saveButton = $('.save-button');
+var $title = $('.title-input');
+var $body = $('.body-input');
+
+$saveButton.click(appendIdea);
+
+function appendIdea() {
+  $('.idea-section').prepend(`<article><h2>${$title.val()}</h2><p>${$body.val()}</p></article>`)
+};
 
