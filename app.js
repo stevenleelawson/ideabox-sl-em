a9e77b627164b9de187c07ad54dd2e40b0a2ad86
Steven Lawson <unluckygamblermusic@gmail.com>
2017-12-17 11:20:33 -0700

    adding delete button svg and js functinality. Need recent work we did yesterday via styling cards

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var $saveButton = $('.save-button');
 var $title = $('.title-input');
 var $body = $('.body-input');
+var $ideaCard = $('.idea-card');
+var $ideaSection = $('.idea-section')
 
 $saveButton.click(appendIdea);
 
@@ -13,6 +15,13 @@ function IdeaConstructor(title, body, quality) {
 
 function appendIdea() {
   // var id = uniqueId();
-  $('.idea-section').append(`<article class="idea-card"><h2>${$title.val()}</h2><button class="delete">delete</button><p>${$body.val()}</p></article><p>${$}`);
+  $('.idea-section').append(`<article class="idea-card">
+  <h2>${$title.val()}</h2>
+  <input type="image" src="images/delete.svg" class="delete"></button><p>${$body.val()}</p></article><p>`);
 
 };
+
+$ideaSection.on('click', '.delete', function(e){
+  $(this).parent().remove();
+  console.log(e);
+})
