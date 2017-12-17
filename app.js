a5d8c729f0f2c274b599678ad4b1caf33b559506
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2017-12-16 17:16:23 -0700

    Add basic responsivensss to upvote button

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var $title = $('.title-input');
 var $body = $('.body-input');
 
 $saveButton.click(appendIdea);
+$('.idea-section').on('click', '.upvote', toggleQuality);
+// $downvote.click(toggleQuality);
 
 function IdeaConstructor(title, body, quality) {
   this.title = title;
   this.body = body;
   this.id = Date.now();
   this.quality = quality || 'swill';
-}
+};
 
 function appendIdea() {
   // var id = uniqueId();
-  $('.idea-section').append(`<article class="idea-card"><h2>${$title.val()}</h2><button class="delete">delete</button><p>${$body.val()}</p></article><p>${$}`);
+  $('.idea-section').append(`<article class="idea-card"><div class="card-header"><h2>${$title.val()}</h2><button class="delete">delete</button></div><p class="idea-body">${$body.val()}</p><p><button class="upvote">upvote</button>quality: "swill"</p></article>`);
+};
 
+function toggleQuality() {
+  console.log('swill')
 };
