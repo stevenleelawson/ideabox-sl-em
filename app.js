9ac3f677270d3fd00408b7e60a0d5b298ad71059
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2017-12-18 14:30:23 -0700

    Add quality upvote functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var $title = $('.title-input');
 var $body = $('.body-input');
 
 $saveButton.click(appendIdea);
-$('.idea-section').on('click', '.upvote', toggleQuality);
+$('.idea-section').on('click', '.upvote', upvoteButton);
 // $downvote.click(toggleQuality);
 
 function IdeaConstructor(title, body, quality) {
@@ -15,9 +15,19 @@ function IdeaConstructor(title, body, quality) {
 
 function appendIdea() {
   // var id = uniqueId();
-  $('.idea-section').append(`<article class="idea-card"><div class="card-header"><h2>${$title.val()}</h2><button class="delete">delete</button></div><p class="idea-body">${$body.val()}</p><p><button class="upvote">upvote</button>quality: "swill"</p></article>`);
+  $('.idea-section').append(`<article class="idea-card">
+                            <div class="card-header"><h2>${$title.val()}</h2><button class="delete">delete</button></div>
+                            <p class="idea-body">${$body.val()}</p>
+                            <button class="upvote">upvote</button>
+                            <p class="quality">swill</p>
+                            </article>`);
 };
 
-function toggleQuality() {
-  console.log('swill')
+function upvoteButton() {
+  var quality = $(this).parent().find('.quality').text();
+  if (quality === 'swill') {
+    $(this).parent().find('.quality').text("plausible");
+  } else {
+    $(this).parent().find('.quality').text('genius');
+  }
 };
