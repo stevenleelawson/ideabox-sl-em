d2cd2e764fedfe8045ed9c125fc19fdc4b3b4c15
Steven Lawson <unluckygamblermusic@gmail.com>
2017-12-21 18:09:45 -0700

    adding functional editable local storage

--- app.js
+++ app.js
@@ -90,12 +90,99 @@ $ideaSection.on('click', '.delete', function(e){
 
 $ideaSection.on('blur', '.card', function(e){
   var cardId = $(this).attr('id');
+  //set item by it's unique id, set in constructor object
+  // console.log(cardId)
   var pulledObject = localStorage.getItem(cardId);
+  //once we have the items id, get it from localStorage
+  // console.log(pulledObject)
   var parsedObject = JSON.parse(pulledObject);
+  //translate back into javaScript
+  // console.log(parsedObject)
   var userTitle = $(this).find('.title').text();
+  //grab and set value for whatever user types into title
+  // console.log(userTitle);
+  // console.log(parsedObject.title)
   parsedObject.title = userTitle;
-  var stringedObject = JSON.stringify(parsedObject)
-  console.log(stringedObject)
+  //reassign the parsed object title to that of what the user types in
+  // console.log(userTitle)
+  var stringedObject = JSON.stringify(parsedObject);
+  // console.log(stringedObject);
   localStorage.setItem(cardId, stringedObject);
-  // console.log(this.updateId)
 })
+
+$ideaSection.on('blur', '.card', function(e){
+  var cardId = $(this).attr('id');
+  // grab item by its id using this, double check what this is
+  // console.log(this)
+  // console.log(cardId);
+  var pulledObject = localStorage.getItem(cardId);
+  //you are pulling a JSON object from localStorage that has already been stored when card is created in createIdea
+  // console.log(pulledObject)
+  var parsedObject = JSON.parse(pulledObject);
+  // console.log(parsedObject)
+  //retrievedObject is assigned to the parsed parsedObject
+  //this is now back in good ol JS, INSTEAD of JSON
+  var userBodyText = $(this).find('.body-text').text();
+  // console.log(userBodyText)
+  // saving to a variable what is already on the card when it was created and now can be edited by the user thanks to edit content = true on html element
+  parsedObject.body = userBodyText;
+  // console.log(userBodyText)
+  //reassinging the parsed object title we got from local storage way back when to what the user is typing/trying to edit
+  var stringifiedObject = JSON.stringify(parsedObject);
+  // console.log(stringifiedObject);
+  localStorage.setItem(cardId, stringifiedObject);
+  //lastly, setting updated item in local storage using it's key and ready to be stored stringified object!!!! You did it, wolf, thanks to Sabrina...
+})
+
+// 1. select parent section and use the event blur which activates when tabbed out of element. opposite of focus
+// 2. set variable to grab attribute of card ID; console log this as a sanity check
+// 3. pull object from local storage to be edited and set to varible(first)
+// 4. destringify object from local storage
+// 5. set variable to grab what edit user types in. (eg title)
+// 6. set the title of object to the title user typed in
+// 7.set var to hold stringified object so as to put into localStorage
+// 8. set item in local storage using object's (id, stringed object variable)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $ideaSection.on('blur', '.card', function(e){
+//   var cardId = $(this).attr('id');
+//   var pulledObject = localStorage.getItem(cardId);
+//   var parsedObject = JSON.parse(pulledObject);
+//   var userTitle = $(this).find('.title').text();
+//   parsedObject.title = userTitle;
+//   var stringedObject = JSON.stringify(parsedObject)
+//   console.log(stringedObject)
+//   localStorage.setItem(cardId, stringedObject);
+//   // console.log(this.updateId)
+// })
